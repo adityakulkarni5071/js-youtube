018b94d76994f45c55ab29a36eeafbad2dd8d25c
Aditya Shriram Kulkarni <adityakulkarni5071@gmail.com>
2025-05-22 10:24:20 +0000

    successful commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+//accountId = 2
+accountEmail = "adit@dfn"
+accountPassword = "2345"
+accountCity = "kop"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/Test.js
@@ -0,0 +1,2 @@
+console.log("Hii I'm Aditya")
+console,log("How are you")
